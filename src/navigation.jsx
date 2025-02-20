3a2f9b689ce6d134de26725f92a73f5e7caeb015
Shailen Varsani <mr.s.varsani@gmail.com>
2025-02-20 10:46:46 +0000

    Adding hover state and icons to buttons

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -57,40 +57,19 @@ export function StickyNavbar() {
     <div>
       <Navbar className="fixed top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
         <div className="flex items-center justify-between text-blue-gray-900">
-          <Typography
-            as="span"
-            variant="h4"
-            href="#"
-            color="black"
-            className="mr-4 cursor-pointer py-1.5 font-normal"
-          >
-            Shailen Varsani
-          </Typography>
+          <Typography as="span" variant="h4" href="#" color="black" className="mr-4 cursor-pointer py-1.5 font-normal">Shailen Varsani</Typography>
           <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
-            <div className="flex items-center gap-x-1">
-              <Button
-                variant="button"
-                size="sm"
-                className="hidden lg:inline-block lg:flex gap-2"
-              >
-                <span>Github</span>
-                <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={2}
-          stroke="currentColor"
-          className="h-5 w-5"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-          />
-        </svg>
+            <div className="flex justify-between gap-x-1">
+              <Button variant="button" size="sm" className="button__small hidden lg:inline-block lg:flex gap-2">
+                <span className="">Github</span>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                  <path fillRule="evenodd" d="M4.25 5.5a.75.75 0 0 0-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 0 0 .75-.75v-4a.75.75 0 0 1 1.5 0v4A2.25 2.25 0 0 1 12.75 17h-8.5A2.25 2.25 0 0 1 2 14.75v-8.5A2.25 2.25 0 0 1 4.25 4h5a.75.75 0 0 1 0 1.5h-5Z" clipRule="evenodd" />
+                  <path fillRule="evenodd" d="M6.194 12.753a.75.75 0 0 0 1.06.053L16.5 4.44v2.81a.75.75 0 0 0 1.5 0v-4.5a.75.75 0 0 0-.75-.75h-4.5a.75.75 0 0 0 0 1.5h2.553l-9.056 8.194a.75.75 0 0 0-.053 1.06Z" clipRule="evenodd" />
+                </svg>
               </Button>
             </div>
+
             <IconButton
               variant="text"
               className="menu-icon ml-auto h-8 w-8 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
